b5b489288af2b0b6b0ffaab313dc12c5a37ba2a0
Tanveerhusyn <tanveerhussain465@gmail.com>
2024-01-12 04:39:51 +0500

    env

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,6 +1,8 @@
 export const environments = {
   // socketURL: "http://localhost:8080",
 
-  apiUrl: 'http://localhost:5000',
-  chatApiUrl: 'http://localhost:5001'
+  // apiUrl: 'http://localhost:5000',
+  apiUrl: 'http://18.224.251.249:5000',
+  // chatApiUrl: 'http://localhost:5001'
+  chatApiUrl: 'http://18.224.251.249:5001'
 };
