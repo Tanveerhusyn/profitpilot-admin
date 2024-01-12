039f86630c367e61f098ba1fee635a9509540a25
Tanveerhusyn <tanveerhussain465@gmail.com>
2024-01-12 07:37:48 +0500

    url update

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,7 +2,7 @@ export const environments = {
   // socketURL: "http://localhost:8080",
 
   // apiUrl: 'http://localhost:5000',
-  apiUrl: 'http://18.224.251.249:5000',
+  apiUrl: 'https://99ba-18-222-17-27.ngrok-free.app',
   // chatApiUrl: 'http://localhost:5001'
   chatApiUrl: 'http://18.224.251.249:5001'
 };
