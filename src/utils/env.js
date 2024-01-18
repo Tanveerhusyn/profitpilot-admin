89606d4a2494ae72760119e707e74d43feb3412d
tanveerhusyn <tanveerhussain465@gmail.com>
2024-01-19 02:36:28 +0500

    env updated

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,8 +1,8 @@
 export const environments = {
   // socketURL: "http://localhost:8080",
 
-  apiUrl: 'http://18.222.17.27:5000',
+  apiUrl: 'https://backend.profitpilotsmb.com',
   // apiUrl: 'https://99ba-18-222-17-27.ngrok-free.app',
   // chatApiUrl: 'http://localhost:5001'
-  chatApiUrl: 'http://18.222.17.27:5001'
+  chatApiUrl: 'https://chat.profitpilotsmb.com'
 };
